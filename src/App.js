5a447e02560edfab6923c5c657cbf90419e46a72
MasonManning <MasonManning@protonmail.com>
2020-09-22 21:20:27 +1000

    Add redux action and increment and decrement the counter on Click

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {useSelector, useDispatch} from 'react-redux'
+import {increment, decrement} from './actions'
 
 function App() {
+  const counter = useSelector(state => state)
+  const dispatch = useDispatch()
   return (
     <div className="App">
      Redux Test
+     <br/>
+     <button onClick={() => dispatch(increment())}>+</button>
+     <button onClick={() => dispatch(decrement())}>-</button>
+     {counter}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -8,6 +8,9 @@ import counter from './reduces/counter'
 import { Provider } from 'react-redux'
 
 const store = createStore(counter, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+store.dispatch({
+  type:'INCREMENT'
+})
 
 
 ReactDOM.render(
